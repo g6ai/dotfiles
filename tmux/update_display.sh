fa59223041af6f2b786350ba090e2ab7363e319d
G. Bai <gdbaifdu@gmail.com>
2020-11-09 17:43:40 +0000

    Revert "Update update_display.sh"
    This reverts commit ada07ac2916fd06f6a26ff210d5278b6a758fe82.

--- tmux/update_display.sh
+++ tmux/update_display.sh
@@ -7,7 +7,7 @@ while read pane_process
 do
     IFS=' ' read -ra pane_process <<< "$pane_process"
     if [[ "${pane_process[1]}" == "bash" ]]; then
-        tmux send-keys -t ${pane_process[0]} "eval $(tmux showenv -s DISPLAY)" Enter
+        tmux send-keys -t ${pane_process[0]} "export DISPLAY=$DISPLAY" Enter
     elif [[ "${pane_process[1]}" == *"vim"* ]]; then
         tmux send-keys -t ${pane_process[0]} Escape
         tmux send-keys -t ${pane_process[0]} ":let \$DISPLAY = \"$DISPLAY\"" Enter
