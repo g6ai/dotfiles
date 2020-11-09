2c7cb52806abd4d520cfb065f3efe81f360132ef
G. Bai <gdbaifdu@gmail.com>
2020-11-09 17:29:11 +0000

    Update update_display.sh

--- tmux/update_display.sh
+++ tmux/update_display.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+DISPLAY=$(tmux show-env | sed -n 's/^DISPLAY=//p')
+
 tmux list-panes -s -F "#{session_name}:#{window_index}.#{pane_index} #{pane_current_command}" | \
-    grep -E "bash" | \
-    cut -f 1 -d " " | \
-    xargs -I PANE tmux send-keys -t PANE 'eval $(tmux showenv -s DISPLAY)' Enter
+while read pane_process
+do
+    IFS=' ' read -ra pane_process <<< "$pane_process"
+    if [[ "${pane_process[1]}" == "bash" ]]; then
+        tmux send-keys -t ${pane_process[0]} "export DISPLAY=$DISPLAY" Enter
+    elif [[ "${pane_process[1]}" == *"vim"* ]]; then
+        tmux send-keys -t ${pane_process[0]} Escape
+        tmux send-keys -t ${pane_process[0]} ":let \$DISPLAY = \"$DISPLAY\"" Enter
+        tmux send-keys -t ${pane_process[0]} ":xrestore" Enter
+    fi
+done
