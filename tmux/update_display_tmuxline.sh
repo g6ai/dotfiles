c10a938076367284685c53b6689a743f4818aa38
G. Bai <gdbaifdu@gmail.com>
2021-06-05 00:47:26 +0100

    Update 'update' Bash scripts for tmux

--- tmux/update_display_tmuxline.sh
+++ tmux/update_display_tmuxline.sh
@@ -2,19 +2,17 @@
 
 DISPLAY=$(tmux show-env | sed -n 's/^DISPLAY=//p')
 
-tmux list-panes -s -F "#{session_name}:#{window_index}.#{pane_index} #{pane_current_command}" | \
-while read pane_process
-do
-  IFS=' ' read -ra pane_process <<< "$pane_process"
-  if [[ "${pane_process[1]}" == "bash" ]]; then
-    tmux send-keys -t ${pane_process[0]} "export DISPLAY=$DISPLAY" Enter
-    tmux if-shell "test -f ~/.config/tmux/tmuxline_iceberg.snapshot" "source ~/.config/tmux/tmuxline_iceberg.snapshot" Enter
-  elif [[ "${pane_process[1]}" == *"vim"* ]]; then
-    tmux send-keys -t ${pane_process[0]} Escape
-    tmux send-keys -t ${pane_process[0]} ":let \$DISPLAY = \"$DISPLAY\"" Enter
-    tmux send-keys -t ${pane_process[0]} ":xrestore" Enter
-    tmux send-keys -t ${pane_process[0]} ":Tmuxline airline" Enter
-  else
-    tmux if-shell "test -f ~/.config/tmux/tmuxline_iceberg.snapshot" "source ~/.config/tmux/tmuxline_iceberg.snapshot" Enter
-  fi
-done
+tmux list-panes -s -F "#{session_name}:#{window_index}.#{pane_index} #{pane_current_command}" \
+  | while read pane_line
+    do
+      IFS=' ' read -ra pane_fields <<< "$pane_line"
+      if [[ "${pane_fields[1]}" == "bash" ]]; then
+        tmux send-keys -t ${pane_fields[0]} "export DISPLAY=$DISPLAY" Enter
+      elif [[ "${pane_fields[1]}" == *"vim"* ]]; then
+        tmux send-keys -t ${pane_fields[0]} Escape
+        tmux send-keys -t ${pane_fields[0]} ":let \$DISPLAY = \"$DISPLAY\"" Enter
+        #tmux send-keys -t ${pane_fields[0]} ":xrestore" Enter
+      fi
+    done
+
+tmux run-shell ~/.config/tmux/update_tmuxline.sh
--- tmux/update_tmuxline.sh
+++ tmux/update_tmuxline.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-tmux list-panes -s -F "#{session_name}:#{window_index}.#{pane_index} #{pane_current_command}" | \
-while read pane_process
-do
-  IFS=' ' read -ra pane_process <<< "$pane_process"
-  if [[ "${pane_process[1]}" == *"vim"* ]]; then
-    tmux send-keys -t ${pane_process[0]} Escape
-    tmux send-keys -t ${pane_process[0]} ":Tmuxline airline" Enter
-  else
-    tmux if-shell "test -f ~/.config/tmux/tmuxline_iceberg.snapshot" "source ~/.config/tmux/tmuxline_iceberg.snapshot" Enter
-  fi
-done
+pane_current_command=$(tmux display -p -F "#{pane_current_command}")
+
+if [[ "$pane_current_command" == *"vim"* ]]; then
+  tmux send-keys Escape
+  tmux send-keys ":Tmuxline airline" Enter
+else
+  tmux if-shell "test -f ~/.config/tmux/tmuxline_iceberg.snapshot" "source ~/.config/tmux/tmuxline_iceberg.snapshot" Enter
+fi
