9c85e96bb4368c70b57f974b359d2be16a3940f8
G. Bai <gdbaifdu@gmail.com>
2021-11-04 16:15:06 +0000

    Add Org-roam-UI config

--- emacs/doom_macos/config.el
+++ emacs/doom_macos/config.el
@@ -220,6 +220,21 @@
     (interactive)
     (my:insert-after 'org-roam-node-insert))
   (map! :leader
-        "n r i" #'my:org-roam-node-insert))
+        "n r i" #'my:org-roam-node-insert)
   (setq +org-roam-open-buffer-on-find-file nil)
 
+  ;; Org-roam-UI
+  (use-package! websocket
+    :after org-roam)
+
+  (use-package! org-roam-ui
+    :after org-roam ;; or :after org
+    ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+    ;;         a hookable mode anymore, you're advised to pick something yourself
+    ;;         if you don't care about startup time, use
+    ;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t)))
