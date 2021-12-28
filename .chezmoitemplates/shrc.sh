645d7000bd32ab5fb7d68f6fdba20581909a1fe1
G. Bai <gdbaifdu@gmail.com>
2021-12-28 18:01:39 +0000

    Use Go text/template and sprig for shell configs

--- .chezmoitemplates/shrc.sh
+++ .chezmoitemplates/shrc.sh
@@ -64,3 +64,32 @@ export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
 
 # man
 export MANPAGER='less -s -M +Gg'
+
+{{ if eq .os "darwin" -}}
+alias cafe="caffeinate -disu &"
+alias bup="brew update && brew upgrade"
+alias lacie="smartctl -a disk3 | grep Temperature"
+alias yd="youtube-dl"
+alias cm="chezmoi"
+
+export PATH="/usr/local/sbin:$PATH"
+
+export PATH="/usr/local/opt/jpeg-turbo/bin:$PATH"
+
+eval "$(pyenv init - --no-rehash)"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+export PYENV_HOOK_PATH="$HOME/.config/pyenv.d"
+
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+if [ -n "$BASH_VERSION" ]; then
+  eval "$(thefuck --alias)"
+fi
+
+export PATH="$HOME/.emacs.d/bin:$PATH"
+
+export PATH="/Library/TeX/texbin:$PATH"
+
+{{ end -}}
