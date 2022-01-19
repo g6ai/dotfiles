5d1be0b295df4339eab0275a26e8190b5a303041
G. Bai <gdbaifdu@gmail.com>
2022-01-19 13:30:17 +0000

    Add tty

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -58,6 +58,7 @@
 
        :os
        (:if IS-MAC macos)  ; improve compatibility with macOS
+       tty               ; improve the terminal Emacs experience
 
        :lang
        emacs-lisp        ; drown in parentheses
