342f8efa5a81ebe9aa626237b3d4d710e601752f
G. Bai <gdbaifdu@gmail.com>
2022-03-30 16:44:44 +0100

    Add my/org-cite-insert function and key mapping

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -291,4 +291,9 @@
          `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
            (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
            (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
-  (setq! citar-symbol-separator "  "))
+  (setq! citar-symbol-separator "  ")
+  (defun my/org-cite-insert ()
+   (interactive)
+   (my/insert-after 'org-cite-insert))
+  (map! :leader
+        "m @" #'my/org-cite-insert))
