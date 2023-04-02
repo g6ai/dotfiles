abef831e37d799fae61f911069f3cb641dc0ad36
G. Bai <gdbaifdu@gmail.com>
2023-04-02 22:30:29 +0100

    Pin transient and with-editor for Emacs 28
    See https://github.com/doomemacs/doomemacs/issues/6425#issuecomment-1433245212.

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -54,3 +54,11 @@
 ;; Org-roam
 (unpin! org-roam)
 (package! org-roam-ui)
+
+(package! transient
+      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+      :recipe (:host github :repo "magit/transient"))
+
+(package! with-editor
+          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+          :recipe (:host github :repo "magit/with-editor"))
