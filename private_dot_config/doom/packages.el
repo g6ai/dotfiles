3be877f90e2a10a5efc36d2da8e887c00300071b
G. Bai <gdbaifdu@gmail.com>
2024-06-23 21:29:47 +0100

    Archive Magit pinning

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -56,13 +56,15 @@
 (unpin! org-roam)
 (package! org-roam-ui)
 
-(package! transient
-      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
-      :recipe (:host github :repo "magit/transient"))
+;; Magit
+;;(package! transient
+;;      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+;;      :recipe (:host github :repo "magit/transient"))
+;;
+;;(package! with-editor
+;;          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+;;          :recipe (:host github :repo "magit/with-editor"))
 
-(package! with-editor
-          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
-          :recipe (:host github :repo "magit/with-editor"))
 ;; Gruvbox
 (package! gruvbox-theme)
 
